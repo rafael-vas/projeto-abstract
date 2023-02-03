494403e35f2d0f5d56a026b37e4d84221f8abb0d
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2023-02-02 22:40:37 -0300

    update logo

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ const menu = document.querySelector(".topbar-button");
 const menuIcone = document.querySelector(".topbar-button img");
 const buttons = document.querySelector(".topbar-controls-wrapper");
 
-
 menu.addEventListener("click", () => {
     buttons.classList.toggle("hidden")
     if (!buttons.classList.contains("hidden")) {
